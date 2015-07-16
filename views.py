e8bb83064ed1e2325776f1d7d0c0cd823bdd7c3e
Anyhowclick <howeilun@hotmail.com>
2015-07-16 14:43:41 +0000

    Improved styling & validation

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
+import datetime
 
 def home(request):
     template = 'index.html'
@@ -9,13 +10,17 @@ def home(request):
 
 def search(request):
     if request.method == 'POST':
+        today = datetime.date.today().isoformat()
         going = request.POST.get("Dday")
         coming = request.POST.get("Aday")
         destination = request.POST.get("selected")
-        if not destination or coming < going:
-            messages.error(request,"Trying to be funny eh!")
-            return render(request, "fail.html")
+        if coming < going:
+            messages.error(request,"Can't come back if you're not there first! Check the dates!")
+            return HttpResponseRedirect('/')
+        elif today > going:
+            messages.error(request,"This isn't a time machine! Check the dates!")
+            return HttpResponseRedirect('/')
         message = going + coming + destination
-        return HttpResponse(message)
+        return render(request, "search.html")
     else:
         return redirect('/')
